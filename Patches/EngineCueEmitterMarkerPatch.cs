4e1cef34f8e9d9e4906fce15bc1f9b45186a4ee6
Pingopete <9340910+Pingopete@users.noreply.github.com>
2026-05-31 21:05:36 -0400

    Mark engine emitters by played cue

---
+++ Patches/EngineCueEmitterMarkerPatch.cs
@@ -0,0 +1,33 @@
+using HarmonyLib;
+using Sandbox.Game.Entities;
+using VRage.Audio;
+
+namespace RealisticSoundPlus.Patches
+{
+    [HarmonyPatch]
+    internal static class EngineCueEmitterMarkerPatch
+    {
+        [HarmonyPatch(typeof(MyEntity3DSoundEmitter), "PlaySound", new[] { typeof(MySoundPair), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool?), typeof(bool) })]
+        [HarmonyPrefix]
+        private static void BeforePlaySoundPair(MyEntity3DSoundEmitter __instance, MySoundPair soundId)
+        {
+            Mark(__instance, soundId?.ToString());
+        }
+
+        [HarmonyPatch(typeof(MyEntity3DSoundEmitter), "PlaySoundWithDistance", new[] { typeof(MyCueId), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool?) })]
+        [HarmonyPrefix]
+        private static void BeforePlaySoundWithDistance(MyEntity3DSoundEmitter __instance, MyCueId soundId)
+        {
+            Mark(__instance, soundId.ToString());
+        }
+
+        private static void Mark(MyEntity3DSoundEmitter emitter, string cueName)
+        {
+            if (emitter == null || !EngineAudioClassifier.IsKnownEngineCue(cueName))
+                return;
+
+            ThrusterFilterPatch.MarkKnownEngineCueEmitter(emitter);
+            AudioDiagnostics.RecordEmitter(emitter, "marked", emitter.VolumeMultiplier, 1f, 1f, emitter.VolumeMultiplier, emitter.SourcePosition);
+        }
+    }
+}
